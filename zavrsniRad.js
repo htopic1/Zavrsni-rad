3924132c0e7c6cf925061e39ba998f9659b1f04a
Haris Topić <haristopic@gmail.com>
2020-02-17 14:38:44 +0100

    Add files via upload

--- zavrsniRad.js
+++ zavrsniRad.js
@@ -1,5 +1,73 @@
 var prviAngular=angular
                 .module('modul',[])
+
+                .controller('meni',function($scope)
+                {
+                    $scope.padajEvropa=function()
+                    {
+                        $scope.styleEvropa=
+                        {
+                            "display":"block"
+                        }
+                    }
+
+                    $scope.sakrijEvropa=function()
+                    {
+                        $scope.styleEvropa=
+                        {
+                            "display":"none"
+                        }
+                    }
+
+                    $scope.padajAfrika=function()
+                    {
+                        $scope.styleAfrika=
+                        {
+                            "display":"block"
+                        }
+                    }
+
+                    $scope.sakrijAfrika=function()
+                    {
+                        $scope.styleAfrika=
+                        {
+                            "display":"none"
+                        }
+                    }
+
+                    $scope.padajAzija=function()
+                    {
+                        $scope.styleAzija=
+                        {
+                            "display":"block"
+                        }
+                    }
+
+                    $scope.sakrijAzija=function()
+                    {
+                        $scope.styleAzija=
+                        {
+                            "display":"none"
+                        }
+                    }
+
+                    $scope.padajAmerika=function()
+                    {
+                        $scope.styleAmerika=
+                        {
+                            "display":"block"
+                        }
+                    }
+
+                    $scope.sakrijAmerika=function()
+                    {
+                        $scope.styleAmerika=
+                        {
+                            "display":"none"
+                        }
+                    }
+                })
+
                 .controller('kontinenti',function($scope)
                 {
                     $scope.evropa=function()
